7f9a8b0a8a261e23618032bc1119a32adc7d2e2b
1tenko <tajm.ogunbiyi@gmail.com>
2022-05-26 22:00:50 +0100

    :slightly_smiling_face: Can now add new books to library and can delete books

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-const cards = document.querySelector('.cards')
-
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -26,9 +24,63 @@ addBookToLibrary('Breaking the Habit of Being Yourself', 'Dr. Joe Dispenza', 368
 addBookToLibrary('Deep Work', 'Cal Newport', 304, true)
 addBookToLibrary('Beyond Order', 'Jordan B. Peterson', 432, false)
 
-myLibrary.forEach(book => {
+
+const cards = document.querySelector('.cards')
+
+function createCard(book) {
     const div = document.createElement('div');
+    const closeCard = document.createElement('div');
+
     div.classList.add('card');
     div.textContent = book.info();
     cards.insertAdjacentElement("afterbegin", div);
+
+    closeCard.classList.add('closeCard');
+    closeCard.textContent = '+';
+    div.insertAdjacentElement("afterbegin", closeCard);
+
+    closeCard.addEventListener('click', () => {
+        div.remove();
+    })
+}
+
+function displayBook() {
+    myLibrary.forEach(book => createCard(book));
+}
+
+displayBook();
+
+
+const fixedButton = document.querySelector('.fixed-button');
+const bgModal = document.querySelector('.bg-modal');
+const close = document.querySelector('.close');
+
+fixedButton.addEventListener('click', () => {
+    bgModal.style.display = 'flex';
+});
+
+close.addEventListener('click', () => {
+    bgModal.style.display = 'none';
+})
+
+
+
+const form = document.querySelector('#submit')
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    let newTitle = document.querySelector('#title').value;
+    let newAuthor = document.querySelector('#author').value;
+    let newPages = document.querySelector('#pages').value;
+    let newReadYes = document.querySelector('#yes').checked;
+    console.log(newReadYes);
+    addBookToLibrary(newTitle, newAuthor, newPages, newReadYes);
+    cards.innerHTML = '';
+    displayBook();
+    bgModal.style.display = 'none';
+    
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
+    document.querySelector('#pages').value = '';
+    
 })
